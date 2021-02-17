1334bbc3ff67ba57f9f3f05c352f5e8192d21307
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-02-17 20:21:57 +0100

    Start clean project

---
+++ LazyBear/LazyBear/LazyBearApp.swift
@@ -0,0 +1,20 @@
+//
+//  LazyBearApp.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 17/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LazyBearApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
