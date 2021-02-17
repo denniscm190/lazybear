ef9eb5ed623e88a512a655982ff64f82a219c46f
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-02-17 13:23:47 +0100

    Clean commit. Start over

--- lazybear/ContentView.swift
+++ lazybear/ContentView.swift
@@ -8,35 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject var apiManager: ApiManager  // Api info
-    let persistenceController = PersistenceController.shared // Core Data
 
     var body: some View {
-        TabView {
-        // First view
-            Search()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                .environmentObject(apiManager)
-                
-            .tabItem {
-                Image(systemName: "magnifyingglass")
-                Text("Search")
-            }
-        
-        // Second view
-            Watchlist()
-            .tabItem {
-                Image(systemName: "list.bullet")
-                Text("Watchlist")
-            }
-        
-        // Third view
-            Settings()
-            .tabItem {
-                Image(systemName: "gear")
-                Text("Settings")
-            }
-        }
+        Text("Hello World")
     }
 }
 
--- lazybear/LazyBearApp.swift
+++ lazybear/LazyBearApp.swift
@@ -9,14 +9,10 @@ import SwiftUI
 
 @main
 struct LazyBearApp: App {
-    let persistenceController = PersistenceController.shared // Core Data
-    var apiManager = ApiManager() // For Cloudkit request
 
     var body: some Scene {
         WindowGroup {
-            AppView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)  // Pass Core Data
-                .environmentObject(apiManager)  // Pass Api info
+            ContentView()
         }
     }
 }
