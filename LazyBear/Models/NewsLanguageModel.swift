ef6fca7cf0acc32376da68ba0599863b55c74c4f
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-02-26 20:01:03 +0000

    Add languages settings

---
+++ LazyBear/Models/NewsLanguageModel.swift
@@ -0,0 +1,14 @@
+//
+//  NewsLanguageModel.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 26/2/21.
+//
+
+import SwiftUI
+
+struct NewsLanguageModel: Codable, Hashable {
+    var name: String
+    var code: String
+    var flag: String
+}
---
+++ LazyBear/UI/NewsLanguagePicker.swift
@@ -0,0 +1,43 @@
+//
+//  NewsLanguagePicker.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 26/2/21.
+//
+
+import SwiftUI
+
+struct NewsLanguagePicker: View {
+    @Environment(\.managedObjectContext) private var moc
+    @State var language: String
+    
+    var body: some View {
+        Picker("News language", selection: $language) {
+            ForEach(newsLanguages, id: \.self) { language in
+                Text("\(language.flag) \(language.name)")
+                    .tag(language.code)
+            }
+        }
+        .onChange(of: language, perform: { language in
+            save(change: language)
+        })
+    }
+    
+    private func save(change: Any) {
+        let userSettings = UserSettings(context: moc)
+        userSettings.changedAt = Date()
+        userSettings.newsLanguage = change as? String
+        do {
+            try moc.save()
+            print("Settings saved")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct NewsLanguagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsLanguagePicker(language: "en")
+    }
+}
