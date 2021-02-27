06ee9af42052eb4eda32d9d52e61edf53ee0dc95
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-02-27 12:45:26 +0000

    Add DateSelection

--- LazyBear/UI/DateSelection.swift
+++ LazyBear/UI/DateSelection.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct DateSelection: View {
+    var period: [String]
+    @Binding var selectedperiod: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Picker(selection: $selectedperiod, label: Text("Please choose a period")) {
+            ForEach(0 ..< period.count) {
+                Text(self.period[$0])
+            }
+        }
+         .pickerStyle(SegmentedPickerStyle())
     }
 }
 
 struct DateSelection_Previews: PreviewProvider {
     static var previews: some View {
-        DateSelection()
+        DateSelection(period: ["1W", "1M", "3M", "6M", "1Y", "2Y", "5Y"], selectedperiod: .constant(0))
     }
 }
