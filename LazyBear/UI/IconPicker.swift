2b2e539e693f67201699322028bedc6c3b68d1d1
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-02-27 12:45:03 +0000

    Update IconPicker

--- LazyBear/UI/IconPicker.swift
+++ LazyBear/UI/IconPicker.swift
@@ -10,9 +10,33 @@ import SwiftUI
 struct IconPicker: View {
     var body: some View {
         List(icons, id:\.name) { icon in
-            IconRow(icon: icon)
+            Button(action: { changeIcon(key: icon.file) }) {
+                HStack {
+                    Image(icon.file)
+                        .resizable()
+                        .cornerRadius(20)
+                        .frame(width: 70, height: 70)
+                        
+                    Text(icon.name)
+                }
+            }
+            .buttonStyle(PlainButtonStyle())
         }
         .listStyle(GroupedListStyle())
+    }
+    
+    private func changeIcon(key: String) {
+        if key != "defaultIcon" {
+            UIApplication.shared.setAlternateIconName(key) { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("Success!")
+                }
+            }
+        } else {
+            UIApplication.shared.setAlternateIconName(nil)
+        }
     }
 }
 
