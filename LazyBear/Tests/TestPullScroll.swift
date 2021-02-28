3afb6373ce3db96dee185a8943c4ff885f99c9f9
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-02-28 18:09:40 +0000

    Delete tests

---
+++ LazyBear/Tests/TestPullScroll.swift
@@ -0,0 +1,20 @@
+//
+//  TestPullScroll.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 28/2/21.
+//
+
+import SwiftUI
+
+struct TestPullScroll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestPullScroll_Previews: PreviewProvider {
+    static var previews: some View {
+        TestPullScroll()
+    }
+}
