25206019f87b16d815925fc90665cb150a981337
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-03-14 13:25:09 +0100

    Add Tests chart

--- LazyBear/Tests/DemoChart.swift
+++ LazyBear/Tests/DemoChart.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct DemoChart: View {
+    let sampleData: [Double] = [10, 11.2, 13.4, 10.2, 13.4, 12.4, 15.6, 18.7, 20.9, 21.2, 10.3]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let normalizedData = normalize(sampleData)
+        Path { path in
+            path.move(to: CGPoint(x: 0, y: 0))
+            for point in normalizedData {
+                path.addLine(to: CGPoint(x: 1, y: point))
+            }
+            
+        }
+        .fill(Color.green)
+    }
+    
+    func normalize(_ data: [Double]) -> [Double] {
+        var normalData = [Double]()
+        let min = data.min()!
+        let max = data.max()!
+        
+        for value in data {
+            let normal = (value - min) / (max - min)
+            normalData.append(normal)
+        }
+        
+        return normalData
     }
 }
 
