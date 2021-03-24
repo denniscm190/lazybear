353586220268d6616476f2f194943630bf46bb07
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-03-24 23:10:35 +0100

    Add Core Data classes

---
+++ LazyBear/Models/WatchlistCompany+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WatchlistCompany+CoreDataClass.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 24/3/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WatchlistCompany)
+public class WatchlistCompany: NSManagedObject {
+
+}
---
+++ LazyBear/Models/WatchlistCompany+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  WatchlistCompany+CoreDataProperties.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 24/3/21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension WatchlistCompany {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<WatchlistCompany> {
+        return NSFetchRequest<WatchlistCompany>(entityName: "WatchlistCompany")
+    }
+
+    @NSManaged public var symbol: String?
+    @NSManaged public var name: String?
+    @NSManaged public var watchlist: String?
+
+}
+
+extension WatchlistCompany : Identifiable {
+
+}
