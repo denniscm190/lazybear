754007ddfaff9f8a2fe0a3e45edd06266591ad12
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-03-27 00:45:57 +0100

    Implementing AvatarCreator

---
+++ LazyBear/Views/Onboarding/Helpers/Avatar.swift
@@ -0,0 +1,20 @@
+//
+//  Avatar.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 26/3/21.
+//
+
+import SwiftUI
+
+struct Avatar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Avatar_Previews: PreviewProvider {
+    static var previews: some View {
+        Avatar()
+    }
+}
---
+++ LazyBear/Views/Onboarding/Helpers/AvatarCreator.swift
@@ -0,0 +1,20 @@
+//
+//  AvatarCreator.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 26/3/21.
+//
+
+import SwiftUI
+
+struct AvatarCreator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AvatarCreator_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatarCreator()
+    }
+}
