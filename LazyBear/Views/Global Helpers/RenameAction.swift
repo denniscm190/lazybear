754e57dbea73330631cdd2cc311ac73aace1cd46
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-04-24 00:41:08 +0200

    Delete HelperView

---
+++ LazyBear/Views/Global Helpers/RenameAction.swift
@@ -0,0 +1,20 @@
+//
+//  RenameAction.swift
+//  LazyBear
+//
+//  Created by Dennis Concepción Martín on 24/4/21.
+//
+
+import SwiftUI
+
+struct RenameAction: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RenameAction_Previews: PreviewProvider {
+    static var previews: some View {
+        RenameAction()
+    }
+}
