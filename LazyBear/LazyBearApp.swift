cf481fa70332c8306f80d2d362d8b84e180a7f53
Dennis Concepción Martín <66180929+denniscm190@users.noreply.github.com>
2021-04-24 12:20:57 +0200

    Delete HudManager.swift

--- LazyBear/LazyBearApp.swift
+++ LazyBear/LazyBearApp.swift
@@ -10,14 +10,11 @@ import SwiftUI
 @main
 struct LazyBearApp: App {
     let persistenceController = PersistenceController.shared  // Core Data init
-    
-    @ObservedObject var hudManager = HudManager()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                .environmentObject(hudManager)
         }
     }
 }
