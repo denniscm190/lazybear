f234b86e297a4ad48be254d966120ebae1089f93
Dennis Concepción Martín <dennisconcepcionmartin@gmail.com>
2021-06-20 12:36:55 +0200

    Delete CompanyView and Helpers

--- LazyBear/Views/Company/CompanyView.swift
+++ LazyBear/Views/Company/CompanyView.swift
@@ -2,57 +2,19 @@
 //  CompanyView.swift
 //  LazyBear
 //
-//  Created by Dennis Concepción Martín on 8/5/21.
+//  Created by Dennis Concepción Martín on 20/6/21.
 //
 
 import SwiftUI
-import StockCharts
 
 struct CompanyView: View {
-    var symbol: String
-    @ObservedObject var company = Company()
-    @State private var showViewSelector = false
-    
-    /*
-     Views
-     */
-    @State private var showChartView = true
-    @State private var showInsiderView = false
-    
     var body: some View {
-        ScrollView {
-            VStack {
-                CompanyHeader(symbol: symbol, showViewSelector: $showViewSelector)
-                    .padding(.bottom)
-
-                if showChartView {
-                    Chart(company: company, symbol: symbol)
-                } else if showInsiderView {
-                    Insiders(company: company, symbol: symbol)
-                }
-            }
-            .padding()
-        }
-        .actionSheet(isPresented: $showViewSelector) {
-            ActionSheet(title: Text("Select an option"), buttons: [
-                .default(Text("Chart & News")) { resetViews(); showChartView = true },
-                .default(Text("Insiders")) { resetViews(); showInsiderView = true },
-                .cancel()
-            ])
-        }
-    }
-    
-    /*
-     Hide all views to show the one selected by user
-     */
-    private func resetViews() {
-        showChartView = false
-        showInsiderView = false
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct CompanyView_Previews: PreviewProvider {
     static var previews: some View {
-        CompanyView(symbol: "AAPL")
+        CompanyView()
     }
 }
